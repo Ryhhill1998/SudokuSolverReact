351b4de9a1c4be1dddb60eb2018592a5df90c6e4
ryhhill1998 <ryhhill1998@outlook.com>
2022-12-02 06:21:04 +0000

    Added new test board

--- src/App.js
+++ src/App.js
@@ -18,6 +18,20 @@ const boardDefault = [
   [0, 0, 0, 0, 7, 0, 9, 0, 0],
 ];
 
+const board1 = [
+  [3, 4, 0, 0, 0, 0, 0, 0, 0],
+  [0, 5, 0, 0, 9, 0, 0, 0, 0],
+  [0, 0, 0, 3, 0, 0, 9, 2, 6],
+  [0, 0, 0, 1, 0, 2, 7, 3, 0],
+  [0, 3, 0, 0, 7, 9, 0, 0, 0],
+  [7, 0, 4, 0, 0, 0, 0, 1, 0],
+  [4, 2, 0, 9, 0, 0, 5, 6, 0],
+  [0, 0, 5, 0, 8, 7, 0, 0, 0],
+  [0, 0, 0, 0, 0, 4, 0, 9, 0],
+];
+
+const boards = [boardDefault, board1];
+
 const App = () => {
   const [board, setBoard] = useState(boardDefault);
 
@@ -26,13 +40,23 @@ const App = () => {
     setBoard(solution);
   };
 
+  const generateNewBoard = () => {
+    const randomIndex = Math.floor(Math.random() * 2);
+    setBoard(boards[randomIndex]);
+  };
+
   return (
     <div className="app-container">
       <h1>SUDOKU SOLVER</h1>
       <Grid board={board} />
-      <button className="solve-button" onClick={printSolvedBoard}>
-        Solve
-      </button>
+      <div>
+        <button className="button generate-button" onClick={generateNewBoard}>
+          Generate
+        </button>
+        <button className="button solve-button" onClick={printSolvedBoard}>
+          Solve
+        </button>
+      </div>
     </div>
   );
 };
